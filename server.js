c89abb5c6c50f9830bf81bc83c7bb81e5ca923d5
lost-in-Code-au <reece.c.jones@gmail.com>
2018-01-19 12:41:21 +0000

    boilerplate graphql, now to work on relay and react

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
+
+const app = express()
+
+app.get('/',(reg, res) => {
+  res.send('GraphQL & Relay modern is cool!!!')
+})
+
+class Friend {
+  constructor(id, {firstName, lastName, gender, language, email}) {
+    this.id = id
+    this.firstName = firstName
+    this.lastName = lastName
+    this.gender = gender
+    this.language = language
+    this.email = email
+  }
+}
+
+const friendDatabase = {
+
+}
+
+const global = {
+  getFriend: ({id})=> {
+    return new Friend(id, friendDatabase[id])
+  },
+  createFriend: ({input}) => {
+    let id = require('crypto').randomBytes(10).toString('hex')
+    friendDatabase[id] = input
+    return new Friend(id, input)
+  },
+  updateFriend: ({id, input}) => {
+    friendDatabase[id] = input
+    return new Friend(id, input)
+  }
+}
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: global,
+  graphiql: true,
+}))
+
+app.listen(3000, () => console.log('Running server on local host 3000/graphql'))
